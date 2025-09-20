72babc99ea790fcfe2c1c91acabec060671b06ad
jayasri <jayasrip953@gmail.com>
2025-09-20 11:51:31 +0530

    added clear_cache() before each run

--- common/time_loop.h
+++ common/time_loop.h
@@ -1,4 +1,11 @@
 #include "../parlay/internal/get_time.h"
+#include <vector>
+#include <cstddef>
+
+void clear_cache() {
+  std::vector<char> arr(40*1024*1024);
+  for (auto& x : arr) x=x;
+}
 
 template<class F, class G, class H>
 void time_loop(int rounds, double delay, F initf, G runf, H endf) {
@@ -6,10 +13,11 @@ void time_loop(int rounds, double delay, F initf, G runf, H endf) {
   // run for delay seconds to "warm things up"
   // will skip if delay is zero
   while (t.total_time() < delay) {
-    initf(); runf(); endf();
+    initf(); clear_cache(); runf(); endf();
   } 
   for (int i=0; i < rounds; i++) {
     initf();
+    clear_cache();
     t.start();
     runf();
     t.next("");
